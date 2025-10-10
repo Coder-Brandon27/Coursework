47ad017668c30aa63a988195e1be6ed870cf5c9f
brandon <Brandonlafferty@hotmail.co.uk>
2025-10-10 16:35:49 +0100

    created all tables as classes for ablity to query in java

---
+++ src/main/java/com/napier/devops/Countrylanguage.java
@@ -0,0 +1,47 @@
+package com.napier.devops;
+
+
+public class Countrylanguage {
+
+  private String countryCode;
+  private String language;
+  private String isOfficial;
+  private double percentage;
+
+
+  public String getCountryCode() {
+    return countryCode;
+  }
+
+  public void setCountryCode(String countryCode) {
+    this.countryCode = countryCode;
+  }
+
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public void setLanguage(String language) {
+    this.language = language;
+  }
+
+
+  public String getIsOfficial() {
+    return isOfficial;
+  }
+
+  public void setIsOfficial(String isOfficial) {
+    this.isOfficial = isOfficial;
+  }
+
+
+  public double getPercentage() {
+    return percentage;
+  }
+
+  public void setPercentage(double percentage) {
+    this.percentage = percentage;
+  }
+
+}
