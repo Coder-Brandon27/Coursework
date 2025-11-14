b6e5e8d721814df1ab75c432f62d250e5e63f007
Jamie-Rutherford <40646520@live.napier.ac.uk>
2025-11-14 09:33:01 +0000

    Update CityReportsTests.java with first test

--- src/test/java/com/napier/devops/CityReportsTests.java
+++ src/test/java/com/napier/devops/CityReportsTests.java
@@ -1,1 +1,34 @@
+package com.napier.devops;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CityReportsTest {
+
+    static DatabaseConnector db;
+    static CityReports cityReports;
+
+    // Runs once before all tests
+    @BeforeAll
+    static void init() {
+        db = new DatabaseConnector();
+        Connection con = db.connect();          // connect to database
+        assertNotNull(con);                     // make sure connection worked
+        cityReports = new CityReports(con);     // create object to test
+    }
+
+    // Test: null country should return an empty list
+    @Test
+    void getCitiesByCountry_NullCountry_ReturnsEmptyList() {
+        List<City> cities = cityReports.getCitiesByCountry(null);
+
+        assertNotNull(cities);                  // method must not return null
+        assertEquals(0, cities.size());         // should be empty
+    }
+}
 
